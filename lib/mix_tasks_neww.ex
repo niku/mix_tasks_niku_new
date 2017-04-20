833d89ed29ade73e34498206394ea3491577b754
niku <niku@niku.name>
2017-04-20 22:11:48 +0900

    Import project

---
+++ lib/mix_tasks_neww.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.Neww do
+  @moduledoc """
+  Documentation for Mix.Tasks.Neww.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mix.Tasks.Neww.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_tasks_neww_test.exs
@@ -0,0 +1,8 @@
+defmodule Mix.Tasks.NewwTest do
+  use ExUnit.Case
+  doctest Mix.Tasks.Neww
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
